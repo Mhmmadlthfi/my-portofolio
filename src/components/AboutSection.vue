3cb7ddc86154c5143565f6cef1d6e3259fc743a3
Muhammad Luthfi Wahid <mhmmadlthfi54@gmail.com>
2025-04-17 23:16:20 +0700

    Penyesuaian about section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,25 +1,36 @@
 <template>
+
   <section id="about" class="py-5" data-aos="fade-up">
+
     <div class="container">
+
       <h2 class="text-center mb-5 gradient-text">Tentang Saya</h2>
+
       <div
         class="glass p-4 text-center rounded mx-auto"
         style="max-width: 900px"
       >
+
         <p>
-          Saya adalah lulusan Sistem Informasi Universitas Teknologi Yogyakarta
+           Saya adalah lulusan Sistem Informasi Universitas Teknologi Yogyakarta
           tahun 2024 yang memiliki minat besar di dunia teknologi. Selama
           kuliah, saya mendalami berbagai bidang seperti pemrograman,
-          database,big data, machine learning, analisis sistem informasi,
-          e-business, serta desain antarmuka pengguna (UI).
+          database,big data dan data analytics, machine learning, analisis
+          sistem informasi, e-business, serta desain antarmuka pengguna (UI).
         </p>
+
         <p>
-          Saya senang mempelajari hal-hal baru dan mudah beradaptasi dengan
+           Saya senang mempelajari hal-hal baru dan mudah beradaptasi dengan
           perubahan teknologi. Dengan pemahaman yang saya miliki serta semangat
           untuk terus belajar, saya siap berkontribusi dalam dunia profesional
           dan berkembang bersama tim.
         </p>
+
       </div>
+
     </div>
+
   </section>
+
 </template>
+
